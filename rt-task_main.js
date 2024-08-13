4ba3dc47fed0e8f981a886762b4fe3695876c8ad
Simone Poggesi <117054508+spoggesi@users.noreply.github.com>
2024-08-13 16:20:30 +1200

    Update rt-task_main.js

--- rt-task_main.js
+++ rt-task_main.js
@@ -76,7 +76,7 @@ var test = {
 var test_procedure = {
     timeline: [fixation, test],
     timeline_variables: test_stimuli,
-    repetitions: 5,
+    repetitions: 1,
     randomize_order: true
 }
 timeline.push(test_procedure);
