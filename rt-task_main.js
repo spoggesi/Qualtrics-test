11a5618dd00e3f98c7db9291aaf606871969007b
Simone Poggesi <117054508+spoggesi@users.noreply.github.com>
2024-08-13 16:07:29 +1200

    Update rt-task_main.js

--- rt-task_main.js
+++ rt-task_main.js
@@ -56,7 +56,7 @@ var fixation = {
     stimulus: '<div style="font-size:60px;">+</div>',
     choices: jsPsych.NO_KEYS,
     trial_duration: function () {
-        return jsPsych.randomization.sampleWithoutReplacement([250, 500, 750, 1000, 1250, 1500, 1750, 2000], 1)[0];
+        return jsPsych.randomization.sampleWithoutReplacement([1000], 1)[0];
     },
     data: {
         test_part: 'fixation'
