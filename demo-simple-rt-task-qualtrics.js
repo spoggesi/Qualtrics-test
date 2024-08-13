2e9c96d4d18e63750f0439d1d4270158f0146979
Simone Poggesi <117054508+spoggesi@users.noreply.github.com>
2024-08-13 15:21:35 +1200

    Add files via upload

---
+++ demo-simple-rt-task-qualtrics.js
@@ -0,0 +1,85 @@
+Qualtrics.SurveyEngine.addOnload(function () {
+
+    /*Place your JavaScript here to run when the page loads*/
+
+    /* Change 1: Hiding the Next button */
+    // Retrieve Qualtrics object and save in qthis
+    var qthis = this;
+
+    // Hide buttons
+    qthis.hideNextButton();
+
+    /* Change 2: Defining and load required resources */
+    var task_github = "https://kywch.github.io/Simple-RT-Task/"; // https://<your-github-username>.github.io/<your-experiment-name>
+
+    // requiredResources must include all the JS files that demo-simple-rt-task-transformed.html uses.
+    var requiredResources = [
+        task_github + "jspsych-6.1.0/jspsych.js",
+        task_github + "jspsych-6.1.0/plugins/jspsych-html-keyboard-response.js",
+        task_github + "jspsych-6.1.0/plugins/jspsych-image-keyboard-response.js",
+        task_github + "rt-task_main.js"
+    ];
+
+    function loadScript(idx) {
+        console.log("Loading ", requiredResources[idx]);
+        jQuery.getScript(requiredResources[idx], function () {
+            if ((idx + 1) < requiredResources.length) {
+                loadScript(idx + 1);
+            } else {
+                initExp();
+            }
+        });
+    }
+
+    if (window.Qualtrics && (!window.frameElement || window.frameElement.id !== "mobile-preview-view")) {
+        loadScript(0);
+    }
+
+    /* Change 3: Appending the display_stage Div using jQuery */
+    // jQuery is loaded in Qualtrics by default
+    jQuery("<div id = 'display_stage_background'></div>").appendTo('body');
+    jQuery("<div id = 'display_stage'></div>").appendTo('body');
+
+    /* Change 4: Wrapping jsPsych.init() in a function */
+    function initExp() {
+
+        jsPsych.init({
+            timeline: timeline,
+            display_element: 'display_stage',
+            on_finish: function (data) {
+                /* Change 5: Summarizing and save the results to Qualtrics */
+                // summarize the results
+                var trials = jsPsych.data.get().filter({
+                    test_part: 'test'
+                });
+                var correct_trials = trials.filter({
+                    correct: true
+                });
+                var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
+                var rt = Math.round(correct_trials.select('rt').mean());
+
+                // save to qualtrics embedded data
+                Qualtrics.SurveyEngine.setEmbeddedData("accuracy", accuracy);
+                Qualtrics.SurveyEngine.setEmbeddedData("rt", rt);
+
+                /* Change 6: Adding the clean up and continue functions.*/
+                // clear the stage
+                jQuery('#display_stage').remove();
+                jQuery('#display_stage_background').remove();
+
+                // simulate click on Qualtrics "next" button, making use of the Qualtrics JS API
+                qthis.clickNextButton();
+            }
+        });
+    }
+});
+
+Qualtrics.SurveyEngine.addOnReady(function () {
+    /*Place your JavaScript here to run when the page is fully displayed*/
+
+});
+
+Qualtrics.SurveyEngine.addOnUnload(function () {
+    /*Place your JavaScript here to run when the page is unloaded*/
+
+});
