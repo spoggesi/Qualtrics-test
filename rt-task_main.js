0aebfd243fb081a39b81709c56c38ced1b6eaf8c
Simone Poggesi <117054508+spoggesi@users.noreply.github.com>
2024-08-29 15:04:57 +1200

    Update rt-task_main.js

--- rt-task_main.js
+++ rt-task_main.js
@@ -9,7 +9,7 @@ var timeline = [];
 
 /* define instructions trial */
 var instructions = {
-    type: "html-button-response",
+    type: "html-keyboard-response",
     stimulus: "<p>You will now carry out the same task.</p>" +
                 "<p>This time, please respond to whether you associate the word shown with the yoghurt you have just eaten or your experience of eating this yoghurt.</p>" +
                 "<p>There are no incorrect answers, just your opinion.</p>" +
@@ -85,7 +85,7 @@ var test_stimuli = [{
 ];
 
 var fixation = {
-    type: 'html-button-response',
+    type: 'html-keyboard-response',
     stimulus: '<div style="font-size:60px;">+</div>',
     choices: jsPsych.NO_KEYS,
     trial_duration: function () {
